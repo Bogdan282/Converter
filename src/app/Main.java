8e577983fd2c7eed9e7eac15794657085bcf9f51
Bogdan282 <bogdan673876@gmail.com>
2025-04-03 15:41:02 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,17 @@
+package app;
+
+public class Main {
+
+    private static final double CONV_K = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converter App.");
+
+        double kg = 5;
+        double pounds = convKgToPounds(kg);
+        System.out.println("Result is " + pounds + " pounds.");
+    }
+    private static double convKgToPounds(double kg) {
+        return kg * CONV_K;
+    }
+}
